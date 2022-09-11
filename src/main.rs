91532d27149ae73a35e6a1d62cee635812bcf097
Levi Tarr <ivelrrat@gmail.com>
2022-09-10 20:00:15 -0700

    read arg for max games

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 mod game;
 
-use std::process;
+use std::{process, env};
 use game::{Player, Game};
 // use game::BasicPlayer;
 use game::BuysBlue;
 // use game::BuysGreen;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let max_games = args.get(1).map_or(1000, |arg| arg.parse().expect("Enter an integer for max games"));
+
     println!("Quacks of Quedlinburg - Sim");
 
     const MAX_GAMES: i32 = 1000;
     let mut points = 0;
-    for _i in 0..MAX_GAMES {
+    for _i in 0..max_games {
         let mut game = Game {
             name: "Quacks".to_string(),
             // player: Player::new(BasicPlayer {}),
@@ -28,5 +32,5 @@ fn main() {
         // println!("{}", game.player.points);
     }
 
-    println!("AVG: {points}/{MAX_GAMES}={}", points as f64 /MAX_GAMES as f64);
+    println!("AVG: {points}/{max_games}={}", points as f64 /max_games as f64);
 }
